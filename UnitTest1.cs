af63327d65a71a9e765030ff0377c9611a7c4ee4
Oisinquinn00 <118039512+Oisinquinn00@users.noreply.github.com>
2023-12-08 05:20:34 +0000

    Add files via upload

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,11 +1,99 @@
-namespace SentenceTests1
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using validSentenceProgram;
+namespace TestProject2
 {
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestValidSentence()
         {
+            // Arrange
+            string input = "The quick brown fox said hello Mr lazy dog.";
+
+            // Act
+            bool result = validSentence.is_sentence_valid(input);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void TestEmptySentence()
+        {
+            // Arrange
+            string input = "";
+
+            // Act
+            bool result = validSentence.is_sentence_valid(input);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void TestSentenceStartsWithLowercase()
+        {
+            // Arrange
+            string input = "the quick brown fox said \"hello Mr lazy dog.";
+
+            // Act
+            bool result = validSentence.is_sentence_valid(input);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void TestSentenceEndsWithPeriod()
+        {
+            // Arrange
+            string input = "One lazy dog is too few, thirteen is too many.";
+
+            // Act
+            bool result = validSentence.is_sentence_valid(input);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void TestSentenceWithOddQuotationCount()
+        {
+            // Arrange
+            string input = "The quick brown fox said \"hello Mr lazy dog"; 
+
+            // Act
+            bool result = validSentence.is_sentence_valid(input);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void TestSentenceWithNumber()
+        {
+            // Arrange
+            string input = "Are there 11, 12 , or 13 lazy dogs?";
+
+            // Act
+            bool result = validSentence.is_sentence_valid(input);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void TestSentenceWithNoPeriods()
+        {
+            // Arrange
+            string input = "There is no punctation in this sentence";
+
+            // Act
+            bool result = validSentence.is_sentence_valid(input);
+
+            // Assert
+            Assert.IsFalse(result);
         }
     }
 }
